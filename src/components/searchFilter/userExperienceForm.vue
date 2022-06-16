368b85b56e7b65760fc702463042695a151d63f2
sudarshan-bacancy <sudarshan.dey@bacancy.com>
2022-06-16 16:12:35 +0530

    User forms created only.

---
+++ src/components/searchFilter/userExperienceForm.vue
@@ -0,0 +1,110 @@
+<template>
+<b-container
+     class="mt-4"
+     style="box-shadow: 5px 5px 5px 5px gray;" 
+    >
+     <h1>Enter your experience details</h1>
+ <b-form inline>
+ 
+  <b-row>
+    <b-col>
+     <b-form-input
+      id="inline-form-input-name"
+      class="mt-3 mr-sm-2 mb-sm-0"
+      placeholder="Job Title"
+    ></b-form-input>
+  </b-col>
+  <b-col>
+    <b-input-group  class="mt-3 mr-sm-2 mb-sm-0">
+      <b-dropdown variant="light" text="Employment type">
+         <b-dropdown-item> Full time </b-dropdown-item>
+          <b-dropdown-item> Part time </b-dropdown-item>
+          <b-dropdown-item> Internship </b-dropdown-item>
+      </b-dropdown>
+    </b-input-group>
+   </b-col>
+ </b-row>
+
+<b-row>
+    <b-col>
+     <b-form-input
+      id="inline-form-input-name"
+      class="mt-3 mr-sm-2 mb-sm-0"
+      placeholder="Name of Company"
+    ></b-form-input>
+  </b-col>
+  <b-col>
+    <b-input-group  class="mt-3 mr-sm-2 mb-sm-0">
+      <b-form-input id="inline-form-input-username" placeholder="Location(in words)"></b-form-input>
+    </b-input-group>
+   </b-col>
+ </b-row>
+
+ <b-row >
+   <b-col class="mt-3">
+      <label for="inline-form-input-name">Start date: </label>
+  </b-col>
+  <b-col class="mt-3">
+    <label for="inline-form-input-username"> End date: </label>
+    </b-col>
+ </b-row>    
+
+ <b-row>
+   <b-col>
+      <b-form-input
+      id="inline-form-input-name"
+      class="mr-sm-2 mb-sm-0"
+      type="date"
+    ></b-form-input>
+  </b-col>
+  
+  <b-col>
+    <b-input-group  class="mr-sm-2 mb-sm-0">
+     <b-form-input id="inline-form-input-username" placeholder="End date" type="date"></b-form-input>
+    </b-input-group>
+   </b-col>
+
+ </b-row>    
+
+<b-row>
+   <b-col>
+      <b-form-textarea
+      id="textarea"
+      class="mt-3 mr-sm-2 mb-sm-0"
+      placeholder="Enter description..."
+      rows="3"
+      max-rows="6"
+    ></b-form-textarea>
+
+  </b-col>
+ </b-row>
+
+  <b-row>
+    <b-form-checkbox
+      id="checkbox-1"
+      name="checkbox-1"
+      class="mt-3 mr-sm-2 mb-sm-0"
+      value="accepted"
+      >
+      <span style="margin:4px;">Employment Status</span>
+    </b-form-checkbox>
+  </b-row>
+
+  <b-row>
+    <b-col class="mt-3 mb-4">
+     <b-button variant="primary" style="width: inherit" type="Submit"> Submit </b-button>
+    </b-col>
+    <b-col class="mt-3 mb-4">
+     <b-button variant="warning" style="width: inherit"> Reset </b-button>
+    </b-col>
+  </b-row>
+ 
+    </b-form>
+   </b-container>
+
+</template>
+<script>
+ export default { }
+</script>
+<style>
+</style>
---
+++ src/components/searchFilter/userProfileForm.vue
@@ -0,0 +1,76 @@
+<template>
+<b-container
+     class="mt-4"
+     style="box-shadow: 5px 5px 5px 5px gray;" 
+    >
+     <h1>Enter Profile details</h1>
+ <b-form inline>
+ 
+  <b-row>
+    <b-col>
+     <b-form-input
+      id="inline-form-input-name"
+      class="mt-3 mr-sm-2 mb-sm-0"
+      placeholder="UserName"
+    ></b-form-input>
+  </b-col>
+  <b-col>
+    <b-input-group  class="mt-3 mr-sm-2 mb-sm-0">
+      <b-form-input id="inline-form-input-username" placeholder="Contact No"></b-form-input>
+    </b-input-group>
+   </b-col>
+ </b-row>
+<!--
+<b-row >
+   <b-col class="mt-3">
+      <label for="inline-form-input-name"> Profile Pic: </label>
+  </b-col>
+ </b-row> -->    
+
+ <b-row>
+    <b-col>
+  <b-input-group  class="mt-3 mr-sm-2 mb-sm-0">
+  Profile Pic:  
+       <input type="file" class="custom-file-upload"/> 
+    </b-input-group>
+  </b-col>
+  <b-col class="mt-5">
+
+   Gender:  <label class="radio-inline" style="margin:2px;" >
+     <input type="radio" name="gender"/> Male
+     </label> 
+     <label class="radio-inline" style="margin:2px;">
+     <input   type="radio" name="gender"/> Female
+     </label>
+          </b-col>
+       </b-row>
+    
+
+  <b-row>
+    <b-col class="mt-3 mb-4">
+     <b-button variant="primary" style="width: inherit" type="Submit"> Submit </b-button>
+    </b-col>
+    <b-col class="mt-3 mb-4">
+     <b-button variant="warning" style="width: inherit"> Reset </b-button>
+    </b-col>
+  </b-row>
+ 
+    </b-form>
+   </b-container>
+</template>
+<script>
+    export default { }
+</script>
+
+<style scoped>
+.custom-file-upload{
+    display: block;
+    width: inherit;
+    background: #fbfdff;
+    font-size: 16px;
+    border: 2px solid #dde6ef;
+    border-radius: 4px;
+    height: 50px;
+    margin-bottom: 20px;
+}
+</style>
